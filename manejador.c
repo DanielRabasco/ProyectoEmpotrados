35b30b07ed098cd09522f9ef28413027a76c3a82
DanielRabasco <danielrabascogarcia@gmail.com>
2018-11-28 15:50:32 +0100

    Third part implemented

--- manejador.c
+++ manejador.c
@@ -8,7 +8,7 @@
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int minor = 0;
-static int n_llamadas_apertura = 0;
+static int n_aperturas_writemode = 0;
 static dev_t dev;
 static struct cdev cdev;
 static struct class *clase;
@@ -19,22 +19,34 @@ struct info_mydev {
 };
 
 static int device_open(struct inode *inode, struct file *filp) {
-	//struct info_mydev *info_dev = container_of(inode->i_cdev, struct info_mydev, mydev_cdev);
-	//filp->private_data = info_dev;
-	printk("FMODE: %i\n", filp->f_mode);
-	printk("WRITE: %i\n", FMODE_WRITE);
-	printk("READ: %i\n", FMODE_READ);
-	printk("Funcion de apertura");
+	struct info_mydev *info_dev = container_of(inode->i_cdev, struct info_mydev, mydev_cdev);
+	filp->private_data = info_dev;
+
+	//Check that there is only one call to open in write-mode
+	if ((filp->f_mode & FMODE_WRITE) != 0){
+		if (n_aperturas_writemode == 0){
+			n_aperturas_writemode++;
+			printk("WRITE");
+		}else{
+			return -EBUSY;
+		}
+	}
+	printk(KERN_ALERT "Funcion de apertura");
 	return 0;
 }
 
+
 static int device_release(struct inode *inode, struct file *filp) {
-	printk("Funcion de liberacion");
+	//If the open call was in write mode, decrease the counter and allow other write-mode open call
+	if ((filp->f_mode & FMODE_WRITE) != 0){
+		n_aperturas_writemode--;
+	}
+	printk(KERN_ALERT "Funcion de liberacion");
 	return 0;
 }
 
 static ssize_t device_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos) {
-	printk("Funcion de escritura");
+	printk(KERN_ALERT "Funcion de escritura");
 	return count;
 }
 
@@ -51,8 +63,8 @@ static int carga_init(void){
 	cdev_add(&cdev, dev, 1);
 	clase = class_create(THIS_MODULE, "speaker");
 	device_create(clase, NULL, dev, NULL, "intspkr");
-	printk("MAJOR:%i\n",MAJOR(dev));
-	printk("MINOR:%i\n",MINOR(dev));
+	printk(KERN_ALERT "MAJOR:%i\n",MAJOR(dev));
+	printk(KERN_ALERT "MINOR:%i\n",MINOR(dev));
 	return 0;
 }
 
